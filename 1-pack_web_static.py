8ca163b45476f0ec35571b8d23c233b37a6720ee
m00n-die <reginaldrock219@gmail.com>
2023-07-08 17:41:18 -0700

    fix for task 01

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,18 +3,19 @@
 from fabric.api import local
 from time import strftime
 from datetime import date
+import os
 
 
 def do_pack():
     """generates archive from the contents of the web_static"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
     cur_date = strftime("%Y%m%d%H%M%S")
-
+    filename = "versions/web_static_{}.tgz web_static/".format(cur_date)
+    
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(cur_date))
-
-        filename = "versions/web_static_{}.tgz web_static/".format(cur_date)
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
         tar_size = os.stat(filename).st_size
         print("web_static packed: {} -> {} Bytes".format(output, tar_size))
         return "versions/web_static_{}.tgz".format(cur_date)
