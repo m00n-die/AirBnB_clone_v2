63188626edef7317ad194162a391cfacdda990fa
Reginald Amuzu <95586757+m00n-die@users.noreply.github.com>
2023-07-09 01:03:47 +0000

    more typo fixes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,19 +15,19 @@ def do_pack():
         os.mkdir("versions")
     cur_date = datetime.now()
     out = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
+        cur_date.year,
+        cur_date.month,
+        cur_date.day,
+        cur_date.hour,
+        cur_date.minute,
+        cur_date.second
     )
 
     try:
         print("Packing web_static to {}".format(out))
         local("tar -cvzf {} web_static".format(out))
         out_size = os.stat(out).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, out_size))
+        print("web_static packed: {} -> {} Bytes".format(out, out_size))
 
     except Exception as e:
         return None
