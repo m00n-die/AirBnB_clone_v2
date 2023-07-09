6612125a93f124d87f9e63ea250aba08dd8e88d4
Reginald Amuzu <95586757+m00n-die@users.noreply.github.com>
2023-07-09 01:11:28 +0000

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -45,13 +45,13 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/{}".format(filename))
-        run("mkdir -p {}".format(dir_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, dir_path))
-        run("rm -rf /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(dir_path, dir_path))
-        run("rm -rf {}web_static".format(dir_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(dir_path))
+        run("sudo mkdir -p {}".format(dir_path))
+        run("sudo tar -xzf /tmp/{} -C {}".format(filename, dir_path))
+        run("sudo rm -rf /tmp/{}".format(filename))
+        run("sudo mv {}web_static/* {}".format(dir_path, dir_path))
+        run("sudo rm -rf {}web_static".format(dir_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(dir_path))
         print('New version deployed!')
         complete = True
 
