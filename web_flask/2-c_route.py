1393d5bb5e8135fc2a04a21715aae48bdb12bb68
Reginald Amuzu <95586757+m00n-die@users.noreply.github.com>
2023-07-23 23:16:08 +0000

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 """Flask application instance"""
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def hello_hbnb():
-        return "<p>Hello HBNB!</p>"
+    return "<p>Hello HBNB!</p>"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def only_hbnb():
 	return "<p>HBNB</p>"
 
-@app.route('/c/<text>', strict_slashes=False)
+
+@app.route('/c/<text>')
 def show_tex(text):
 	spaced_text = text.replace("_", " ")
 	return f"<p>C { spaced_text }</p>"
+
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port='5000')
